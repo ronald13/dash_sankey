40cf4c44a9c297d3e9768a3964bd07fc7dc09df7
Aliaksandr Ihnatsiuk <a.ihnatsiuk@digital-science.com>
2022-11-14 17:13:21 +0300

    Change functions

--- prep.py
+++ prep.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import plotly.graph_objects as go
 
-def prepare_sahkey_data(data):
-    df = pd.read_csv(data)
+def prepare_sahkey_data(data,names=[], count_dict={}, source_list=[], target_list=[]):
 
+    df = pd.read_csv(data)
     #groups has all the combinations of 6 months
     groups = df.groupby(['First Month','Second Month','Third Month','Fourth Month','Fifth Month','Sixth Month']).agg({'Customer_id':'count'}).reset_index()
     #first_month has all the combinations of 1st Month with 2nd Month
@@ -20,24 +20,25 @@ def prepare_sahkey_data(data):
     list_= [first_month,second_month,third_month,fourth_month,fifth_month]
 
     # names contains all the labels of our nodes. We will add suffix #'_M1,_M2,_M3....' to our segmentation to differntiate one months #segement with other months,i.e LOW VALUE CUSTOMER_M3 tells Low #value customer in 3rd month.
-    names = []
-    count_dict = {}  # will contain all info of value list
-    source_list = []  # will contain all info of source
-    target_list = []  # will contain all info of target
+    # names = []
+    # count_dict = {}  # will contain all info of value list
+    # source_list = []  # will contain all info of source
+    # target_list = []  # will contain all info of target
+
     for i in range(0, len(list_)):
         cols = list_[i].columns  # contains columns for our dataframe
         # (list_[i])
         # This for loop is inside the outer loop
-    for x, y, z in zip(list_[i][cols[0]], list_[i][cols[1]],
-                       list_[i][cols[2]]):  # Iterates over x(source),y(target),z(counts)
+        for x, y, z in zip(list_[i][cols[0]], list_[i][cols[1]],
+                           list_[i][cols[2]]):  # Iterates over x(source),y(target),z(counts)
 
-        if (x + '_M' + str(i + 1) not in names):
-            names.append(x + '_M' + str(i + 1))  # appends in names
-        # the next line is outside the if but inside the second loop
-        count_dict[x + '_M' + str(i + 1), y + '_M' + str(i + 2)] = z
-        source_list.append(x + '_M' + str(i + 1))
-        target_list.append(y + '_M' + str(i + 2))
-    # Now we add labels into name for the last month targets
+            if (x + '_M' + str(i + 1) not in names):
+                names.append(x + '_M' + str(i + 1))  # appends in names
+            # the next line is outside the if but inside the second loop
+            count_dict[x + '_M' + str(i + 1), y + '_M' + str(i + 2)] = z
+            source_list.append(x + '_M' + str(i + 1))
+            target_list.append(y + '_M' + str(i + 2))
+        # Now we add labels into name for the last month targets
     for v in target_list:
         if v not in names:
             names.append(v)
@@ -46,5 +47,4 @@ def prepare_sahkey_data(data):
     all_numerics = {}
     for i in range(0, len(names)):
         all_numerics[names[i]] = i
-
     return all_numerics, names, count_dict, source_list, target_list
