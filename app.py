057f1050abc1f9b341c21382f239b06e9c1839b8
Aliaksandr Ihnatsiuk <a.ihnatsiuk@digital-science.com>
2022-11-14 17:13:42 +0300

    Change dash

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 import os
-import pandas as pd
 from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output
 from charts import create_sankey
-import plotly.graph_objects as go
 import dash_trich_components as dtc
 from prep import prepare_sahkey_data
 
 all_numerics, names, count_dict, source_list, target_list = prepare_sahkey_data('data/Simulated_Customer_Data_Sankey.csv')
-
 
 app = Dash(
     __name__, external_stylesheets=[dbc.themes.BOOTSTRAP],
@@ -30,7 +26,7 @@ header = html.Div([
 ], className='dash__header')
 
 footer = html.Div([
-    html.P('Created by:', style={}),
+    html.P('Source:', style={}),
 
 ], className='dash__footer')
 
@@ -42,7 +38,7 @@ app.layout = html.Div([
                     html.Div([
                           html.P('Customer Segmentation', style={'font-size':'16px', 'font-weight':'600'}),
                           html.P('Month on Month'),
-                          dcc.Graph(figure=create_sankey(all_numerics, names, count_dict, source_list, target_list), style={'width':'100%'})
+                          dcc.Graph(figure=create_sankey(all_numerics, names, count_dict, source_list, target_list ), style={'width':'100%'})
                     ], className='dash__graph_block'),
                     footer
 
